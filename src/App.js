852ada53f6e93ca4ebd6f84e7e42e1e2abe9925c
Smarty <kozhevnikovavaleria91@gmail.com>
2019-06-02 11:35:44 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import WeatherDisplay from "./WeatherDisplay";
+import "bootswatch/dist/slate/bootstrap.css";
+import { Navbar, Nav, Container, Row, Col, NavbarBrand } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            activePlace: 0
+        };
+    }
+    render() {
+        const activePlace = this.state.activePlace;
+        return (
+            <div>
+                <Navbar
+                    style={{marginBottom:'100px'}}
+                >
+                        <NavbarBrand>
+                            React Simple Weather App
+                        </NavbarBrand>
+                </Navbar>
+                <Container>
+                    <Row>
+                        <Col md={4} sm={4}>
+                            <h3>Select a city</h3>
+                            <Nav
+                                className="flex-column"
+                                variant="pills"
+                                stacked="true"
+                                activeKey={activePlace}
+                                onSelect={index => {
+                                    this.setState({ activePlace: index });
+                                }}
+                            >
+                                {PLACES.map((place, index) => (
+                                    <Nav.Link key={index} eventKey={index}>{place.name}</Nav.Link>
+                                ))}
+                            </Nav>
+                        </Col>
+                        <Col md={8} sm={8}>
+                            <WeatherDisplay key={activePlace} zip={PLACES[activePlace].zip} />
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default App;
+
+const PLACES = [
+    { name: "Palo Alto", zip: "94303" },
+    { name: "San Jose", zip: "94088" },
+    { name: "Santa Cruz", zip: "95062" },
+    { name: "Honolulu", zip: "96803" }
+];
